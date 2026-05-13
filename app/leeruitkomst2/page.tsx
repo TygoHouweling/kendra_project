2798d0ab8e1d04e347224eaa05f0ca21b929d110
TygoHo <Tygo@houweling.eu>
2026-05-13 20:12:19 +0200

    sample for leeruitkomsten

--- app/leeruitkomst2/page.tsx
+++ app/leeruitkomst2/page.tsx
@@ -1,8 +1,43 @@
+import Image from "next/image";
+
 export default function leeruitkomst2() {
   return (
-    <div>
+    <div className="mx-[10vw] mb-[5vh]">
       <h1>Leeruitkomst 2</h1>
-      <p>Dit is leeruitkomst 2</p>
+      <h2>Subkopje Leeruitkomst 2</h2>
+      <div className="gap-[5vh] flex-col flex">
+        <div className="flex flex-row justify-between gap-[5vw]">
+          <Image
+            src="/images/stock.jpg"
+            alt="frontpage filler image"
+            className="flex-1 rounded-xl shadow"
+            width="400"
+            height="400"
+            loading="eager"
+          />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, aut doloremque molestiae quia quod odit animi quidem unde, consequatur cum blanditiis, hic earum non minima mollitia fugiat dolores! Eveniet, error?</p>
+        </div>
+        <div className="flex flex-row-reverse justify-between gap-[5vw]">
+          <Image
+            src="/images/stock.jpg"
+            alt="frontpage filler image"
+            className="flex-1 rounded-xl shadow"
+            width="400"
+            height="400"
+            loading="eager"
+          />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, aut doloremque molestiae quia quod odit animi quidem unde, consequatur cum blanditiis, hic earum non minima mollitia fugiat dolores! Eveniet, error?</p>
+        </div>
+        <div className="flex flex-row justify-between gap-[5vw]">
+          <video width="400" height="400" controls preload="eager" className="rounded-xl shadow">
+            <source src="/videos/stock.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, aut doloremque molestiae quia quod odit animi quidem unde, consequatur cum blanditiis, hic earum non minima mollitia fugiat dolores! Eveniet, error?</p>
+        </div>
+
+      </div>
+
     </div>
   )
 }
