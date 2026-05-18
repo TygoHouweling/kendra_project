d04c060123b8baf54a850dc70566eea5a1588125
TygoHo <Tygo@houweling.eu>
2026-05-18 23:37:52 +0200

    update

--- app/(with-bg)/leeruitkomst1/arbeidEnOrganisatie/page.tsx
+++ app/(with-bg)/leeruitkomst1/arbeidEnOrganisatie/page.tsx
@@ -2,21 +2,27 @@ import Image from "next/image";
 
 export default function arbeidEnOrganisatie() {
     return (
-        <div>
-            <h1 className="text-xl bold">Leeruitkomst 1</h1>
-
-            <div className="flex gap">
+        <div className="mx-[10vw] mb-[5vh] gap-[3vh] flex flex-col min-h-full">
+            <h1 className="text-xl bold">Arbeid en Organisatie</h1>
+            <h2>Webinar werkdruk en werkstress de baas</h2>
+            <div className="flex flex-row justify-between gap-[5vw]">
                 <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={100}
-                    height={20}
-                    priority
+                    src="/images/webinar_werkdrukEnWerkstress.png"
+                    alt="frontpage filler image"
+                    className="flex-1 rounded-xl shadow"
+                    width="600"
+                    height="600"
+                    loading="eager"
                 />
-                <p>Hoi dit is mooie tekst</p>
+                <p>Ik ga een online webinar volgen genaamd "werkdruk en werkstress de baas", die gaat over psychosociale arbeidsbelasting (PSA) en hoe daarmee omgegaan kan worden als leidinggevende of preventiemedewerker. Deze webinar sluit aan bij de specialisatie arbeid en organisatie en laat zien dat ik mij hierin heb verdiept. Ik verwerk opgedane kennis in een mindmap.
+                </p>
             </div>
-
+            <p>
+                De webinar sluit aan bij de specialisatie arbeid en organisatie, omdat het gaat over mens en werk. Bij arbeid en organisatie is het doel om mensen te laten floreren in hun werk en organisatie. Dit kan dwarsgezeten worden door werkstress oftewel psychosociale arbeidsbelasting. In de webinar wordt hierop ingegaan, dus wat het inhoudt maar ook hoe hiermee omgegaan kan worden.
+            </p>
+            <p>
+                Hoewel ik het interessant vond om meer te leren over werkstress en psychosociale arbeidsbelasting, merkte ik tijdens de webinar dat mijn interesse minder ligt bij het analyseren en begeleiden van stress- en welzijnsvraagstukken binnen organisaties. Ik vond het nuttig om kennis op te doen over dit onderwerp, maar ik merkte dat mijn motivatie en nieuwsgierigheid sterker uitgaan naar andere thema’s binnen de psychologie.
+            </p>
         </div>
     );
 }
