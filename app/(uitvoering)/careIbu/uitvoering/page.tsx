ace531d438c1531b8be868d9ffbaf9974fe9f4c9
TygoHo <Tygo@houweling.eu>
2026-06-06 23:44:12 +0200

    updated pages

--- app/(uitvoering)/careIbu/uitvoering/page.tsx
+++ app/(uitvoering)/careIbu/uitvoering/page.tsx
@@ -7,70 +7,96 @@ export default function careIbuUitvoering() {
             <p>
                 Ik ga bij woonzorgcentrum de Ingelanden meerdere keren meelopen op de maandag en daar helpen bij de activiteiten en een 1 op 1 maatje begeleiden. Dit doe ik om mij te oriënteren op de specialisatie levensloop. Ik verwerk mijn dagen en ervaringen in een logboek.
             </p>
-            <table className="table-auto rounded-xl border border-gray-200 overflow-hidden shadow-xl">
-                <colgroup>
-                    <col className="w-[25%]" />
-                    <col className="w-[37.5%]" />
-                    <col className="w-[37.5%]" />
-                </colgroup>
-                <thead>
-                    <tr className="bg-[#f1d5cc] border-b border-gray-200">
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Dag
-                        </th>
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Wat heb ik gedaan?
-                        </th>
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Wat heb ik geleerd?
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Donderdag 26 maart</p></td>
-                        <td><p>Kennismaking gehad en een snelle rondleiding</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 30 maart</p></td>
-                        <td><p>Ochtend een documentaire gekeken. In de middag koekjes versieren met de ouderen en kletsen, ook gebruik gemaakt van de beleef tv en geheugenspelletjes gespeeld.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 13 april</p></td>
-                        <td><p>In de ochtend vragenspelletjes gedaan met wat ouderen in mijn eentje. In de middag cakejes gebakken voor de bewoners.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 20 april</p></td>
-                        <td><p>In de ochtend op de duofiets mijn maatje G. In de middag worstenbroodjes gemaakt met de bewoners en ik heb de beleef tv gebruik en begeleiding gegeven bij memory spelen.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 4 mei</p></td>
-                        <td><p>In de ochtend op de duofiets.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4]">
-                        <td><p>Maandag 18 mei</p></td>
-                        <td><p>In de ochtend op de duofiets. In de middag wafels versieren, beleef tv en muziek luisteren en zingen. </p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="overflow-x-auto w-full">
+                <table className="table-auto rounded-xl border border-gray-200 overflow-hidden shadow-xl">
+                    <colgroup>
+                        <col className="w-[25%]" />
+                        <col className="w-[37.5%]" />
+                        <col className="w-[37.5%]" />
+                    </colgroup>
+                    <thead>
+                        <tr className="bg-[#f1d5cc] border-b border-gray-200">
+                            <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
+                                Dag
+                            </th>
+                            <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
+                                Wat heb ik gedaan?
+                            </th>
+                            <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
+                                Wat heb ik geleerd?
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-[#f7ede4] border-b-1">
+                            <td><p>Donderdag 26 maart</p></td>
+                            <td><p>Kennismaking gehad en een snelle rondleiding</p></td>
+                            <td>
+                                <p>
+                                    Ik heb geleerd wat de begrippen NAH en PG inhouden.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4] border-b-1">
+                            <td><p>Maandag 30 maart</p></td>
+                            <td><p>Ochtend een documentaire gekeken. In de middag koekjes versieren met de ouderen en kletsen, ook gebruik gemaakt van de beleef tv en geheugenspelletjes gespeeld.</p></td>
+                            <td>
+                                <p>
+                                    Ik vond het heftig om sommige bewoners te zien, omdat sommigen niet eens konden communiceren of bewegen. Ik leerde dat je bij sommige bewoners je grenzen moet stellen.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4] border-b-1">
+                            <td><p>Maandag 13 april</p></td>
+                            <td><p>In de ochtend vragenspelletjes gedaan met wat ouderen in mijn eentje. In de middag cakejes gebakken voor de bewoners.</p></td>
+                            <td>
+                                <p>
+                                    Aan het begin wilde bijna niemand meedoen met het vragenspel, alleen één bewoner deed mee. Nadat de andere bewoners nog even de kat uit de boom keken, begonnen er steeds meer bewoners mee te spelen. Ik herkende dit patroon ook al van de vorige keer met de beleeftv. Wellicht dat de bewoners pas willen meedoen als ze een eigen keus hebben en niet als ze van alles voorgelegd krijgen.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4] border-b-1">
+                            <td><p>Maandag 20 april</p></td>
+                            <td><p>In de ochtend op de duofiets mijn maatje G. In de middag worstenbroodjes gemaakt met de bewoners en ik heb de beleef tv gebruik en begeleiding gegeven bij memory spelen.</p></td>
+                            <td>
+                                <p>
+                                    Ik heb vandaag aan G gevraagd wat hij fijn vindt met manier hoe ik om moet gaan met zijn vergeetachtigheid. Ik vroeg dit omdat ik dit uit een eerder gastcollege had geleerd. G gaf aan dat hij het vervelend vindt als mensen op een botte manier aangeven als hij al een keer iets heeft gezegd. Het liefst wil hij dat mensen op een normale en beleefde manier zeggen dat hij zichzelf herhaald.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4] border-b-1">
+                            <td><p>Maandag 4 mei</p></td>
+                            <td><p>In de ochtend op de duofiets.</p></td>
+                            <td>
+                                <p>
+                                    Ik heb vandaag weer veel met G gekletst toen we aan het duofietsen waren. Ik leer steeds beter hoe ik om moet gaan met zijn vergeetachtigheid. Aan het begin maakte me het soms ongemakkelijk, maar ik merk steeds meer dat ik minder ongemak ervaar.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4]">
+                            <td><p>Maandag 18 mei</p></td>
+                            <td><p>In de ochtend op de duofiets. In de middag wafels versieren, beleef tv en muziek luisteren en zingen. </p></td>
+                            <td>
+                                <p>
+                                    Tijdens de middag activiteit viel het mij op dat veel van de bewoners muziek wel goed kunnen herinneren. Ondanks dat ze veel dingen vergeten of niet meer kunnen herinneren, kunnen ze de songtekst van veel nummers wel met gemak meezingen ook herkennen ze de melodie. Ik vind dat heel mooi om te zien en wil hier meer onderzoek naar doen.
+                                </p>
+                                <p>
+                                    Ook leerde ik dat als een bewoner naar buiten wil maar dat niet mag, je eerst moet wachten totdat deze bewoner weg is voordat je zelf naar buiten gaat.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr className="bg-[#f7ede4]">
+                            <td><p>Maandag 1 juni</p></td>
+                            <td><p>In de ochtend de voorbereiding voor de middag gedaan, want in de middag gingen we taartjes versieren.</p></td>
+                            <td>
+                                <p>
+                                    Ik ging vandaag voor het eerst zelf bewoners ophalen voor de middagactiviteit. Ik moest hierbij een nieuwe vrijwilliger begeleiden. Dit keer liep ik ook tegen hetzelfde probleem aan als op 13 april. Ik merkte weer dat bewoners weerstand bieden als je ze iets voorlegt, ze willen een eigen keuze kunnen maken. Ook dit keer viel het mij op dat muziek en melodie goed blijft hangen bij de bewoners. Dit waren allemaal bewoners van de pg-afdeling, dus met dementie. Wellicht dat dementie de herinnering van muziek minder aantast? Ik wil hier meer onderzoek naar doen
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
--- app/(uitvoering)/interviewPsycholoog/uitvoering/page.tsx
+++ app/(uitvoering)/interviewPsycholoog/uitvoering/page.tsx
@@ -4,73 +4,7 @@ export default function interviewPsycholoogUitvoering() {
     return (
         <div className="mx-[10vw] mb-[5vh] gap-[3vh] flex flex-col min-h-full">
             <h1 className="text-xl bold">Interview Psycholoog</h1>
-            <p>
-                Ik ga bij woonzorgcentrum de Ingelanden meerdere keren meelopen op de maandag en daar helpen bij de activiteiten en een 1 op 1 maatje begeleiden. Dit doe ik om mij te oriënteren op de specialisatie levensloop. Ik verwerk mijn dagen en ervaringen in een logboek.
-            </p>
-            <table className="table-auto rounded-xl border border-gray-200 overflow-hidden shadow-xl">
-                <colgroup>
-                    <col className="w-[25%]" />
-                    <col className="w-[37.5%]" />
-                    <col className="w-[37.5%]" />
-                </colgroup>
-                <thead>
-                    <tr className="bg-[#f1d5cc] border-b border-gray-200">
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Dag
-                        </th>
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Wat heb ik gedaan?
-                        </th>
-                        <th className="px-4 py-2.5 text-left text-m font-medium tracking-wide">
-                            Wat heb ik geleerd?
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Donderdag 26 maart</p></td>
-                        <td><p>Kennismaking gehad en een snelle rondleiding</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 30 maart</p></td>
-                        <td><p>Ochtend een documentaire gekeken. In de middag koekjes versieren met de ouderen en kletsen, ook gebruik gemaakt van de beleef tv en geheugenspelletjes gespeeld.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 13 april</p></td>
-                        <td><p>In de ochtend vragenspelletjes gedaan met wat ouderen in mijn eentje. In de middag cakejes gebakken voor de bewoners.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 20 april</p></td>
-                        <td><p>In de ochtend op de duofiets mijn maatje G. In de middag worstenbroodjes gemaakt met de bewoners en ik heb de beleef tv gebruik en begeleiding gegeven bij memory spelen.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4] border-b-1">
-                        <td><p>Maandag 4 mei</p></td>
-                        <td><p>In de ochtend op de duofiets.</p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                    <tr className="bg-[#f7ede4]">
-                        <td><p>Maandag 18 mei</p></td>
-                        <td><p>In de ochtend op de duofiets. In de middag wafels versieren, beleef tv en muziek luisteren en zingen. </p></td>
-                        <td>
-                            <p></p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+    
         </div>
     );
 }
