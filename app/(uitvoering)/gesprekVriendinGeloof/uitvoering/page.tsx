ba34845ca8b1ac7b817789dd29fe96d6edc3d9f4
TygoHo <Tygo@houweling.eu>
2026-07-06 03:57:31 +0200

    updated video

--- app/(uitvoering)/gesprekVriendinGeloof/uitvoering/page.tsx
+++ app/(uitvoering)/gesprekVriendinGeloof/uitvoering/page.tsx
@@ -25,7 +25,7 @@ export default function gesprekVriendinGeloofUitvoering() {
             </p>
 
             <video controls width="600">
-                <source src="/videos/gesprek_vriendin_geloof.mp4" type="video/mp4" />
+                <source src="/videos/gesprek_vriendin_geloof_2.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
